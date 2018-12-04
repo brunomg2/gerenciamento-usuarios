f99b3eed05ceb56a1bb1d9e30ad966a0cc6a012f
Bruno Gabriel <brunomg2016@hotmail.com>
2018-12-04 16:47:22 -0300

    novos dados

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,12 +1,16 @@
 const fields = document.querySelectorAll('#form-user-create [name]')
 const user = {}
 
-fields.forEach(function (field, index) {
-    if (field.name == 'gender' && field.checked) {
-        user[field.name] = field.value
-    } else {
-        user[field.name] = field.value
-    }
+document.getElementById('form-user-create').addEventListener('submit', event => {
+    event.preventDefault()
+    
+    fields.forEach(function (field, index) {
+        if (field.name == 'gender' && field.checked) {
+            user[field.name] = field.value
+        } else {
+            user[field.name] = field.value
+        }
+    })
+    
+    console.log(user)
 })
-
-console.log(user)
