836f658490911a0771694aee7bb3ed3f84c4242f
Bruno Gabriel <brunomg2016@hotmail.com>
2019-01-05 11:45:06 -0300

    novas atualizações

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -11,11 +11,18 @@ class UserController {
         this.formEl.addEventListener('submit', event => {
             event.preventDefault()
 
+            const btn = this.formEl.querySelector('[type=submit]')
+            btn.disabled = true
+
             const values = this.getValues()
             this.getPhoto().then(
+
                 result => {
                     values.photo = result
                     this.addLine(values)
+
+                    this.formEl.reset()
+                    btn.disabled = false
                 },
                 error => {
                     console.error(error)
@@ -26,7 +33,7 @@ class UserController {
 
     getPhoto() {
         return new Promise((resolve, reject) => {
-            
+
             const fileReader = new FileReader
             const elements = [...this.formEl.elements].filter(item => {
                 if (item.name === 'photo') {
@@ -44,7 +51,7 @@ class UserController {
                 reject(error)
             }
 
-            if(file) {
+            if (file) {
                 fileReader.readAsDataURL(file)
             } else {
                 resolve('dist/img/boxed-bg.jpg')
@@ -56,18 +63,30 @@ class UserController {
 
         let user = {}
         const elements = [...this.formEl.elements]
+        let isValid = true
+        const requiredData = ['name', 'email', 'password']
 
         elements.forEach((field, index) => {
+            
+            if(requiredData.indexOf(field.name) > -1 && !field.value) {
+                field.parentElement.classList.add('has-error')
+                isValid = false
+            }
+
             if (field.name == 'gender') {
-                if (field.checked) { 
+                if (field.checked) {
                     user[field.name] = field.value
                 }
             } else if (field.name === 'admin') {
                 user[field.name] = field.checked
-            }else {
+            } else {
                 user[field.name] = field.value
             }
         })
+
+        if(!isValid) {
+            return false
+        }
 
         return new User(
             user.name,
@@ -84,7 +103,7 @@ class UserController {
     addLine(dataUser) {
 
         const tr = document.createElement('tr')
-        tr.innerHTML =  
+        tr.innerHTML =
             `
             <td>
             <img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm">
@@ -92,13 +111,13 @@ class UserController {
             <td>${dataUser.name}</td>
             <td>${dataUser.email}</td>
             <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
-            <td>${dataUser.birth}</td>
+            <td>${dataUser.register.toLocaleDateString('pt-br')}</td>
             <td>
             <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
             <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
             </td>
             `
-            this.tableEl.appendChild(tr)
+        this.tableEl.appendChild(tr)
     }
 
 }
